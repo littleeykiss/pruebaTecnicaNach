549c1d6f172011c668de60c05b987dea7f90235d
littleeykiss <er_guad@hotmail.com>
2025-03-14 09:36:28 -0600

    priceServices | 'Se sube actualizacion OpenAPI para la visualizacion del contrato Swagger en el servicio'

--- priceservice/src/main/java/com/example/priceservice/Application.java
+++ priceservice/src/main/java/com/example/priceservice/Application.java
@@ -2,6 +2,10 @@ package com.example.priceservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 
 @SpringBootApplication
 public class Application {
@@ -9,5 +13,13 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Price Service API")
+                        .version("1.0")
+                        .description("API para la gestión de precios"));
+    }
 
 }
